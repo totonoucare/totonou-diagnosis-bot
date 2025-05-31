94afd5c4dc5a6d47d6f7f71cf446bee3bf8e558a
totonoucare <novelinks9@gmail.com>
2025-05-31 18:10:48 +0900

    Update resultGenerator.js

--- diagnosis/resultGenerator.js
+++ diagnosis/resultGenerator.js
@@ -1,7 +1,8 @@
 const resultDictionary = require("./resultDictionary");
 const flowDictionary = require("./flowDictionary");
 const organDictionary = require("./organDictionary");
-const adviceDictionary = require("./adviceDictionary"); // ← 追加
+const adviceDictionary = require("./adviceDictionary");
+const linkDictionary = require("./linkDictionary");
 const getTypeName = require("./typeMapper");
 
 function generateResult(score1, score2, score3, flowType, organType) {
@@ -9,14 +10,16 @@ function generateResult(score1, score2, score3, flowType, organType) {
   const baseInfo = resultDictionary[typeName] || {};
   const flowInfo = flowDictionary[flowType] || "";
   const organInfo = organDictionary[organType] || "";
-  const advice = adviceDictionary[typeName] || ""; // ← 追加
+  const advice = adviceDictionary[typeName] || "";
+  const link = linkDictionary[typeName] || "";
 
   return {
     type: typeName || "不明な体質タイプ",
     traits: baseInfo.traits || "",
     flowIssue: flowInfo,
     organBurden: organInfo,
-    advice: advice, // ← 出力に追加
+    advice: advice,
+    link: link
   };
 }
 
