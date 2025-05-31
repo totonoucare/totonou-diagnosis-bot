04ba2c424b571958978d28a70350ecd5f6fb09a9
totonoucare <novelinks9@gmail.com>
2025-06-01 03:05:06 +0900

    Update stomach_Q2.js

--- diagnosis/stomach_Q2.js
+++ diagnosis/stomach_Q2.js
@@ -1,16 +1,16 @@
-// diagnosis/stomach_Q2.js
-const { buildQuestionFlex } = require('../utils/flexBuilder');
+const { MessageBuilder } = require('../utils/flexBuilder');
 
-module.exports = async (userId, client) => {
-  const flex = buildQuestionFlex({
-    title: 'Q2：お腹の不調が強くなるのは？',
-    questionId: 'Q2',
-    options: [
-      { label: '冷たい飲食／冷房で悪化', value: 'A' },
-      { label: '油っこいもの／胃が熱い感じ', value: 'B' },
-      { label: '特にどちらもない', value: 'C' }
-    ]
+module.exports = async function stomach_Q2() {
+  const flex = MessageBuilder({
+    altText: '【質問2】お腹の不調が強くなるのは？',
+    header: '【Q2】お腹の不調が強くなるのは？',
+    body: '次のうち、あなたに当てはまるものを選んでください。',
+    buttons: [
+      { label: 'A：冷たい飲食／冷房で悪化', data: 'stomach_Q2_A' },
+      { label: 'B：油っこいもの／胃が熱い感じ', data: 'stomach_Q2_B' },
+      { label: 'C：特にどちらもない', data: 'stomach_Q2_C' },
+    ],
   });
 
-  await client.pushMessage(userId, flex);
+  return flex;
 };
