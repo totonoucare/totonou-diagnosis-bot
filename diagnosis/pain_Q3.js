7b1cf2e47814d559c1dcc0fe816775048feaf85d
totonoucare <novelinks9@gmail.com>
2025-06-01 04:22:42 +0900

    Update pain_Q3.js

--- diagnosis/pain_Q3.js
+++ diagnosis/pain_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function pain_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】疲れ方・回復のしやすさ',
     header: '【Q3】疲労感と回復傾向',
-    body: `Q3：体の疲れ方や、回復のしやすさで近いのは？\nB：筋肉がつきにくい／回復が遅い／皮膚や髪が乾燥\nA：疲れやすく、だるさが抜けにくい\nC：どちらもあまり当てはまらない`,
+    body: `Q3：体の疲れ方や、回復のしやすさで近いのは？\nA：筋肉がつきにくい／回復が遅い／皮膚や髪が乾燥\nB：疲れやすく、だるさが抜けにくい\nC：どちらもあまり当てはまらない`,
     buttons: [
       { label: 'A', data: 'pain_Q3_A' },
       { label: 'B', data: 'pain_Q3_B' },
