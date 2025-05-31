4896593e53be7c1c1a352153f455c5ee2b8e472a
totonoucare <novelinks9@gmail.com>
2025-06-01 03:05:55 +0900

    Update stomach_Q3.js

--- diagnosis/stomach_Q3.js
+++ diagnosis/stomach_Q3.js
@@ -1,16 +1,16 @@
-// diagnosis/stomach_Q3.js
-const { buildQuestionFlex } = require('../utils/flexBuilder');
+const { MessageBuilder } = require('../utils/flexBuilder');
 
-module.exports = async (userId, client) => {
-  const flex = buildQuestionFlex({
-    title: 'Q3：お腹の働きについて当てはまるのは？',
-    questionId: 'Q3',
-    options: [
-      { label: '口が渇く／唇が荒れる／便が硬い', value: 'A' },
-      { label: '食欲なし／軟便・下痢ぎみ', value: 'B' },
-      { label: 'どちらもない', value: 'C' }
-    ]
+module.exports = async function stomach_Q3() {
+  const flex = MessageBuilder({
+    altText: '【質問3】お腹の働きについて当てはまるのは？',
+    header: '【Q3】お腹の働きについて当てはまるのは？',
+    body: '次のうち、あなたに当てはまるものを選んでください。',
+    buttons: [
+      { label: 'A：口が渇く／唇が荒れる／便が硬い', data: 'stomach_Q3_A' },
+      { label: 'B：食欲なし／軟便・下痢ぎみ', data: 'stomach_Q3_B' },
+      { label: 'C：どちらもない', data: 'stomach_Q3_C' },
+    ],
   });
 
-  await client.pushMessage(userId, flex);
+  return flex;
 };
