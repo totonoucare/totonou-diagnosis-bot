94cd7647f94efba7b742c959b9b48d46cb29505b
totonoucare <novelinks9@gmail.com>
2025-06-01 04:46:37 +0900

    Create women_Q3.js

---
+++ diagnosis/women_Q3.js
@@ -0,0 +1,16 @@
+const { MessageBuilder } = require('../utils/flexBuilder');
+
+module.exports = async function women_Q3() {
+  const flex = MessageBuilder({
+    altText: '【質問3】普段の体調や見た目',
+    header: '【Q3】日常の体調変化',
+    body: `Q3：普段の体調や見た目で当てはまるのは？\nA：肌や髪がパサつく／めまい・ふらつき\nB：顔色が白い／疲れやすい／声が小さい\nC：どちらも特にない`,
+    buttons: [
+      { label: 'A', data: 'women_Q3_A' },
+      { label: 'B', data: 'women_Q3_B' },
+      { label: 'C', data: 'women_Q3_C' },
+    ],
+  });
+
+  return flex;
+};
