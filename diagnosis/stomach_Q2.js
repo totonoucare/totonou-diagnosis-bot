cf4509bf1b378c261274020790df1477a087b16a
totonoucare <novelinks9@gmail.com>
2025-06-01 04:07:21 +0900

    Update stomach_Q2.js

--- diagnosis/stomach_Q2.js
+++ diagnosis/stomach_Q2.js
@@ -1,10 +1,10 @@
 const { MessageBuilder } = require('../utils/flexBuilder');
 
-module.exports = async (userId, client) => {
+module.exports = async function stomach_Q2() {
   const flex = MessageBuilder({
-    altText: '【Q2】お腹の不調が強くなるのは？',
-    header: 'Q2：お腹の不調が強くなるのは？',
-    body: `次のうち、最も当てはまるものを選んでください。\n\nA：冷たい飲食／冷房で悪化\nB：油っこいもの／胃が熱い感じ\nC：特にどちらもない`,
+    altText: '【質問2】お腹の不調について',
+    header: '【Q2】お腹の不調について',
+    body: `Q2：お腹の不調が強くなるのは？\nA：冷たい飲食のあと／冷房・冷気\nB：油っこい・辛いもの／食後に胃が熱い感じ\nC：どちらも当てはまらない`,
     buttons: [
       { label: 'A', data: 'stomach_Q2_A' },
       { label: 'B', data: 'stomach_Q2_B' },
@@ -12,5 +12,5 @@ module.exports = async (userId, client) => {
     ],
   });
 
-  await client.pushMessage(userId, flex);
+  return flex;
 };
