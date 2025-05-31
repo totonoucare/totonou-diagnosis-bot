0abc63b6e8f9b51613d793ecd94539a27eee4ae3
totonoucare <novelinks9@gmail.com>
2025-06-01 04:46:56 +0900

    Create women_Q4.js

---
+++ diagnosis/women_Q4.js
@@ -0,0 +1,17 @@
+const { MessageBuilder } = require('../utils/flexBuilder');
+
+module.exports = async function women_Q4() {
+  const flex = MessageBuilder({
+    altText: '【質問4】月経時の体調',
+    header: '【Q4】月経と体の反応',
+    body: `Q4：月経にまつわる体調で当てはまるのは？\nA：月経前に張り・胸の違和感・イライラ\nB：月経中に下腹が重だるい／眠い・むくむ\nC：月経時の強い痛み／塊が出る／経血が暗い\nD：どれも特にない`,
+    buttons: [
+      { label: 'A', data: 'women_Q4_A' },
+      { label: 'B', data: 'women_Q4_B' },
+      { label: 'C', data: 'women_Q4_C' },
+      { label: 'D', data: 'women_Q4_D' },
+    ],
+  });
+
+  return flex;
+};
