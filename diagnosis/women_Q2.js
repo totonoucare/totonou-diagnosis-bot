aa996bfb214ed2e1eda342d0773486aa7d33d647
totonoucare <novelinks9@gmail.com>
2025-06-01 04:46:17 +0900

    Create women_Q2.js

---
+++ diagnosis/women_Q2.js
@@ -0,0 +1,16 @@
+const { MessageBuilder } = require('../utils/flexBuilder');
+
+module.exports = async function women_Q2() {
+  const flex = MessageBuilder({
+    altText: '【質問2】冷え・のぼせ',
+    header: '【Q2】冷えやのぼせの傾向',
+    body: `Q2：冷えやのぼせについて、当てはまるのは？\nA：全身が冷える／下腹部が冷たい\nB：上半身や顔がほてる／寝汗・のぼせ・イライラ\nC：とくに感じない`,
+    buttons: [
+      { label: 'A', data: 'women_Q2_A' },
+      { label: 'B', data: 'women_Q2_B' },
+      { label: 'C', data: 'women_Q2_C' },
+    ],
+  });
+
+  return flex;
+};
