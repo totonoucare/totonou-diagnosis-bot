2b42f3c25cbd6da82f548f3b635172980460b3db
totonoucare <novelinks9@gmail.com>
2025-06-01 03:32:12 +0900

    Update stomach_Q1.js

--- diagnosis/stomach_Q1.js
+++ diagnosis/stomach_Q1.js
@@ -1,16 +1,16 @@
 const { MessageBuilder } = require('../utils/flexBuilder');
 
-module.exports = async function stomach_Q1() {
+module.exports = async (userId, client) => {
   const flex = MessageBuilder({
-    altText: '【質問1】食事の量について教えてください',
-    header: '【Q1】食事の量について',
-    body: '次のうち、あなたの普段の食事の量に近いものを選んでください。',
+    altText: '【Q1】食事の量について',
+    header: 'Q1：食事の量について',
+    body: `次のうち、あなたの普段の食事の量に近いものを選んでください。\n\nA：少食で満腹になりやすい\nB：しっかり食べる方\nC：日によって差がある`,
     buttons: [
-      { label: 'A：少食で満腹になりやすい', data: 'stomach_Q1_A' },
-      { label: 'B：しっかり食べる方', data: 'stomach_Q1_B' },
-      { label: 'C：日によって差がある', data: 'stomach_Q1_C' },
+      { label: 'A', data: 'stomach_Q1_A' },
+      { label: 'B', data: 'stomach_Q1_B' },
+      { label: 'C', data: 'stomach_Q1_C' },
     ],
   });
 
-  return flex;
+  await client.pushMessage(userId, flex);
 };
