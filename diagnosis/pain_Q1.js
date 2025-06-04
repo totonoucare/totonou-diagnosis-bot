1466e3fcfb6c044d859f5c5f31cfe4092ac57d29
totonoucare <novelinks9@gmail.com>
2025-06-04 20:24:05 +0900

    Update pain_Q1.js

--- diagnosis/pain_Q1.js
+++ diagnosis/pain_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function pain_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】痛みが出るタイミング',
     header: '【Q1】痛みが出るタイミングやきっかけ',
-    body: `Q1：痛みが出るタイミングやきっかけで近いのは？\nA：疲れるとズーンと重だるくなる\nB：動かすと痛む／押すと痛い\nC：どちらとも言えない`,
+    body: `Q1：痛みが出るタイミングやきっかけで近いのは？\n\nA：疲れるとズーンと重だるくなる\nB：動かすと痛む／押すと痛い\nC：どちらとも言えない`,
     buttons: [
       { label: 'A', data: 'pain_Q1_A' },
       { label: 'B', data: 'pain_Q1_B' },
