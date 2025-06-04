7d2b998dd555fa1a9815b18bdfda27816f3c3e79
totonoucare <novelinks9@gmail.com>
2025-06-04 20:22:13 +0900

    Update common_Q3.js

--- diagnosis/common_Q3.js
+++ diagnosis/common_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やエネルギー感について',
     header: '【Q3】体力やエネルギー感について',
-    body: `Q3：体力やエネルギー感について、最も近いのはどれですか？\nA：夢が多い／めまい・立ちくらみ／肌・髪が乾燥\nB：疲れやすい／息切れしやすい／声に力がない\nC：どちらもあまり感じない`,
+    body: `Q3：体力やエネルギー感について、最も近いのはどれですか？\n\nA：夢が多い／めまい・立ちくらみ／肌・髪が乾燥\nB：疲れやすい／息切れしやすい／声に力がない\nC：どちらもあまり感じない`,
     buttons: [
       { label: 'A', data: 'common_Q3_A' },
       { label: 'B', data: 'common_Q3_B' },
