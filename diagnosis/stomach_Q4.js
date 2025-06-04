140ba8687345c37d7d1198c62cf8c65555c8f42c
totonoucare <novelinks9@gmail.com>
2025-06-04 20:31:53 +0900

    Update stomach_Q4.js

--- diagnosis/stomach_Q4.js
+++ diagnosis/stomach_Q4.js
@@ -4,7 +4,7 @@ module.exports = async function stomach_Q4() {
   const flex = MessageBuilder({
     altText: '【質問4】食後のお腹の感じ',
     header: '【Q4】食後のお腹の感じ',
-    body: `Q4：食後のお腹の感じに近いのは？\nA：パンパンに張る・ガスが溜まる感じ\nB：ズーンと重くてだるい・胃がもたれる\nC：差し込む痛み・黒っぽい便\nD：特に気にならない`,
+    body: `Q4：食後のお腹の感じに近いのは？\n\nA：パンパンに張る・ガスが溜まる感じ\nB：ズーンと重くてだるい・胃がもたれる\nC：差し込む痛み・黒っぽい便\nD：特に気にならない`,
     buttons: [
       { label: 'A', data: 'stomach_Q4_A' },
       { label: 'B', data: 'stomach_Q4_B' },
