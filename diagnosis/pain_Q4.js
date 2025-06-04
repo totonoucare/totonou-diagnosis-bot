94b1bc022e8cdbd688bb00d0d22cabe59f56b516
totonoucare <novelinks9@gmail.com>
2025-06-04 20:26:06 +0900

    Update pain_Q4.js

--- diagnosis/pain_Q4.js
+++ diagnosis/pain_Q4.js
@@ -4,7 +4,7 @@ module.exports = async function pain_Q4() {
   const flex = MessageBuilder({
     altText: '【質問4】痛みの質や出方',
     header: '【Q4】痛みの質や出方',
-    body: `Q4：痛みの質や出方で、近いものは？\nA：張りが強い・朝だけ辛い\nB：重だるさやむくみが強い\nC：刺すような痛み／夜間悪化／押すと強く痛い\nD：どれとも言えない`,
+    body: `Q4：痛みの質や出方で、近いものは？\n\nA：張りが強い・朝だけ辛い\nB：重だるさやむくみが強い\nC：刺すような痛み／夜間悪化／押すと強く痛い\nD：どれとも言えない`,
     buttons: [
       { label: 'A', data: 'pain_Q4_A' },
       { label: 'B', data: 'pain_Q4_B' },
