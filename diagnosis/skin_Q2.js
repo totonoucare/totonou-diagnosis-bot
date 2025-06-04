8bcbe796f85adad76b28a8f6d19a221a0d48ee86
totonoucare <novelinks9@gmail.com>
2025-06-04 20:28:50 +0900

    Update skin_Q2.js

--- diagnosis/skin_Q2.js
+++ diagnosis/skin_Q2.js
@@ -4,7 +4,7 @@ module.exports = async function skin_Q2() {
   const flex = MessageBuilder({
     altText: '【質問2】頭皮や肌のトラブルの傾向',
     header: '【Q2】肌・頭皮の冷えや熱',
-    body: `Q2：頭皮や肌のトラブルについて、最近当てはまるのは？\nA：冷えると悪化／冬場や冷房が苦手\nB：赤み・ほてり・かゆみ／夏や入浴後に悪化\nC：特に偏りはない`,
+    body: `Q2：頭皮や肌のトラブルについて、最近当てはまるのは？\n\nA：冷えると悪化／冬場や冷房が苦手\nB：赤み・ほてり・かゆみ／夏や入浴後に悪化\nC：特に偏りはない`,
     buttons: [
       { label: 'A', data: 'skin_Q2_A' },
       { label: 'B', data: 'skin_Q2_B' },
