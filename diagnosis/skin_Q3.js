6eb7b357c35e961c0577d23e3b15df7a95aa4104
totonoucare <novelinks9@gmail.com>
2025-06-04 20:29:08 +0900

    Update skin_Q3.js

--- diagnosis/skin_Q3.js
+++ diagnosis/skin_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function skin_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】髪や肌の状態',
     header: '【Q3】髪や肌の変化',
-    body: `Q3：髪や肌の状態について、近いのは？\nA：髪がパサつく／抜けやすい／肌がくすむ\nB：疲れやすい／息切れ／顔色が白くハリがない\nC：特に気になる変化はない`,
+    body: `Q3：髪や肌の状態について、近いのは？\n\nA：髪がパサつく／抜けやすい／肌がくすむ\nB：疲れやすい／息切れ／顔色が白くハリがない\nC：特に気になる変化はない`,
     buttons: [
       { label: 'A', data: 'skin_Q3_A' },
       { label: 'B', data: 'skin_Q3_B' },
