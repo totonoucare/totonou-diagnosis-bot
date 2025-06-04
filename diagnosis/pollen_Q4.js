46fc9e9fb6468fa2fae9a41b535daf54194551e0
totonoucare <novelinks9@gmail.com>
2025-06-04 20:27:44 +0900

    Update pollen_Q4.js

--- diagnosis/pollen_Q4.js
+++ diagnosis/pollen_Q4.js
@@ -4,7 +4,7 @@ module.exports = async function pollen_Q4() {
   const flex = MessageBuilder({
     altText: '【質問4】鼻や頭の不調',
     header: '【Q4】鼻まわりのつらさ',
-    body: `Q4：鼻や頭まわりの不調として、当てはまるのは？\nA：ずっと詰まっていてイライラ\nB：どろっとした鼻水・頭が重い・だるい\nC：頭が締めつけられる／クマやくすみ／鼻血\nD：とくに当てはまらない`,
+    body: `Q4：鼻や頭まわりの不調として、当てはまるのは？\n\nA：ずっと詰まっていてイライラ\nB：どろっとした鼻水・頭が重い・だるい\nC：頭が締めつけられる／クマやくすみ／鼻血\nD：とくに当てはまらない`,
     buttons: [
       { label: 'A', data: 'pollen_Q4_A' },
       { label: 'B', data: 'pollen_Q4_B' },
