3734b1c3edf59a1af759e6828abd1542b9b38a8c
totonoucare <novelinks9@gmail.com>
2025-06-04 20:31:17 +0900

    Update stomach_Q2.js

--- diagnosis/stomach_Q2.js
+++ diagnosis/stomach_Q2.js
@@ -4,7 +4,7 @@ module.exports = async function stomach_Q2() {
   const flex = MessageBuilder({
     altText: '【質問2】お腹の不調について',
     header: '【Q2】お腹の不調について',
-    body: `Q2：お腹の不調が強くなるのは？\nA：冷たい飲食のあと／冷房・冷気\nB：油っこい・辛いもの／食後に胃が熱い感じ\nC：どちらも当てはまらない`,
+    body: `Q2：お腹の不調が強くなるのは？\n\nA：冷たい飲食のあと／冷房・冷気\nB：油っこい・辛いもの／食後に胃が熱い感じ\nC：どちらも当てはまらない`,
     buttons: [
       { label: 'A', data: 'stomach_Q2_A' },
       { label: 'B', data: 'stomach_Q2_B' },
