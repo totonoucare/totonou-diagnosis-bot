539895d6aef10fc916a33cd4429eb693b76b4515
totonoucare <novelinks9@gmail.com>
2025-06-04 20:28:34 +0900

    Update skin_Q1.js

--- diagnosis/skin_Q1.js
+++ diagnosis/skin_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function skin_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】抜け毛や肌トラブルのタイミング',
     header: '【Q1】肌や髪の不調が起きやすい時期',
-    body: `Q1：抜け毛や肌トラブルが起きやすいタイミングは？\nA：疲れているとき／季節の変わり目に弱い\nB：寝不足やストレスが強いと一気に悪化\nC：どちらでもない`,
+    body: `Q1：抜け毛や肌トラブルが起きやすいタイミングは？\n\nA：疲れているとき／季節の変わり目に弱い\nB：寝不足やストレスが強いと一気に悪化\nC：どちらでもない`,
     buttons: [
       { label: 'A', data: 'skin_Q1_A' },
       { label: 'B', data: 'skin_Q1_B' },
