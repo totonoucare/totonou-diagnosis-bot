1248c3883219b8774a96bbb489c66a8c63399512
totonoucare <novelinks9@gmail.com>
2025-06-07 03:01:29 +0900

    Update common_Q2.js

--- diagnosis/common_Q2.js
+++ diagnosis/common_Q2.js
@@ -4,7 +4,8 @@ module.exports = async function common_Q2() {
   const flex = MessageBuilder({
     altText: '【質問2】冷えやほてりについて',
     header: '【Q2】冷えやほてりについて',
-    body: `Q2：最近、冷えやほてりについて感じることはありますか？\n\nA：全身が冷えやすい／寒がり／冷えると症状が悪化\nB：顔がほてる／寝汗・イライラ／夜に頭が冴える\nC：とくに冷えや熱は気にならない`,
+    body: `Q2：最近、冷えやほてりに関してどちらが多く当てはまりますか？
+（一部当てはまる場合でも、より強く感じるほうを選んでください）\n\nA：全身が冷えやすい／寒がり／冷えると症状が悪化\nB：顔がほてる／寝汗・イライラ／夜に頭が冴える\nC：とくに冷えや熱は気にならない`,
     buttons: [
       { label: 'A', data: 'common_Q2_A' },
       { label: 'B', data: 'common_Q2_B' },
