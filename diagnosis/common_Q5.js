4695b19d9badda0b8e633f0ef1bc98320802e0c3
totonoucare <novelinks9@gmail.com>
2025-06-06 17:49:14 +0900

    Update common_Q5.js

--- diagnosis/common_Q5.js
+++ diagnosis/common_Q5.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q5() {
   const flex = MessageBuilder({
     altText: '【質問5】体の違和感・張り感がある動き',
     header: '【Q5】体の違和感・張り感がある動き',
-    body: `Q5：次の動作の中で、体の違和感や張り感を最も感じるのはどれですか？\n\nA：首を後ろに倒すor左右に回す\nB：腕をバンザイする\nC：前屈する\nD：腰を左右にねじるor側屈\nE：上体をそらす`,
+    body: `Q5：次の動作のうち、体のどこかに一番違和感やツラさを感じる動作を選んでください。\n\nA：首を後ろに倒すor左右に回す\nB：腕をバンザイする\nC：前屈する\nD：腰を左右にねじるor側屈\nE：上体をそらす`,
     buttons: [
       { label: 'A', data: 'common_Q5_A' },
       { label: 'B', data: 'common_Q5_B' },
