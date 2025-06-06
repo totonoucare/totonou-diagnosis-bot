accb64bc4c831cd4e630e555d9382aba4b889eed
totonoucare <novelinks9@gmail.com>
2025-06-06 17:24:46 +0900

    Update pollen_Q1.js

--- diagnosis/pollen_Q1.js
+++ diagnosis/pollen_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function pollen_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】風邪をひいた時のパターン',
     header: '【Q1】風邪のときの傾向',
-    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンはどちらですか？\n\nA：長引きやすい・微熱くらいでグズグズ\nB：高熱が出る・喉や鼻の炎症が強い\nC：その時々で違う`,
+    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：長引きやすい・微熱くらいでグズグズ\nB：高熱が出る・喉や鼻の炎症が強い\nC：その時々で違う`,
     buttons: [
       { label: 'A', data: 'pollen_Q1_A' },
       { label: 'B', data: 'pollen_Q1_B' },
