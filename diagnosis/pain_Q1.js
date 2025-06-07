a5c9276c99e29cf088cc447fffbd0882416374f0
totonoucare <novelinks9@gmail.com>
2025-06-07 15:05:32 +0900

    Update pain_Q1.js

--- diagnosis/pain_Q1.js
+++ diagnosis/pain_Q1.js
@@ -2,10 +2,9 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function pain_Q1() {
   const flex = MessageBuilder({
-    altText: '【質問1】痛みが出るタイミング',
-    header: '【Q1】痛みが出るタイミングやきっかけ',
-    body: `Q1：痛みが出るタイミングやきっかけで近いのは？\n\nA：疲れるとズーンと重だるくなる\nB：動かすと痛む／押すと痛い\nC：どちらとも言えない`,
-    buttons: [
+    altText: '【質問1】ここ数年間の体力傾向',
+    header: '【Q1】ここ数年間の体力傾向',
+    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：軽い症状で長引く／ダラダラ治りきらない\nB：ガッと発症してもすぐ治る／滅多に風邪はひかない（特別な対策なし）\nC：時と場合による／特に傾向はない`,    buttons: [
       { label: 'A', data: 'pain_Q1_A' },
       { label: 'B', data: 'pain_Q1_B' },
       { label: 'C', data: 'pain_Q1_C' },
