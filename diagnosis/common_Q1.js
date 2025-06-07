3cd302a48161190a7e22d47133dac4c19300d3b7
totonoucare <novelinks9@gmail.com>
2025-06-07 14:17:15 +0900

    Update common_Q1.js

--- diagnosis/common_Q1.js
+++ diagnosis/common_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】風邪をひいた時のパターン',
     header: '【Q1】風邪をひいた時のパターン',
-    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：軽い症状で長引く／だらだらしやすい\nB：ガッと発症してもすぐ治る／あまり風邪をひかない\nC：時と場合による／特に傾向はない`,
+    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：軽い症状で長引く／ダラダラ治りきらない\nB：ガッと発症してもすぐ治る／滅多に風邪はひかない（特別な対策なし）\nC：時と場合による／特に傾向はない`,
     buttons: [
       { label: 'A', data: 'common_Q1_A' },
       { label: 'B', data: 'common_Q1_B' },
