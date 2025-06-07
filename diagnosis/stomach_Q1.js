1ba27f9b736005828bb8a7a05fa7e022b95d7e09
totonoucare <novelinks9@gmail.com>
2025-06-07 14:29:28 +0900

    Update stomach_Q1.js

--- diagnosis/stomach_Q1.js
+++ diagnosis/stomach_Q1.js
@@ -2,9 +2,9 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function stomach_Q1() {
   const flex = MessageBuilder({
-    altText: '【質問1】食事の量について',
-    header: '【Q1】食事の量について',
-    body: `Q1：元気なときの食事の量の傾向は？※体調不良時や一時的な食欲の低下は除いてお考えください。\n\nA：少食で満腹になりやすい\nB：比較的しっかり食べる方\nC：日によって差がある`,
+altText: '【質問1】ここ数年間の体力傾向',
+    header: '【Q1】ここ数年間の体力傾向（共通質問）',
+    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：軽い症状で長引く／ダラダラ治りきらない\nB：ガッと発症してもすぐ治る／滅多に風邪はひかない（特別な対策なし）\nC：時と場合による／特に傾向はない`,
     buttons: [
       { label: 'A', data: 'stomach_Q1_A' },
       { label: 'B', data: 'stomach_Q1_B' },
