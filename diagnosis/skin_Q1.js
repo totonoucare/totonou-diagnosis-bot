8d15befa5f08029412961c0ecc0fbf9ab2d58e9a
totonoucare <novelinks9@gmail.com>
2025-06-07 15:09:42 +0900

    Update skin_Q1.js

--- diagnosis/skin_Q1.js
+++ diagnosis/skin_Q1.js
@@ -2,10 +2,9 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function skin_Q1() {
   const flex = MessageBuilder({
-    altText: '【質問1】抜け毛や肌トラブルのタイミング',
-    header: '【Q1】肌や髪の不調が起きやすい時期',
-    body: `Q1：抜け毛や肌トラブルの出かたとして、より当てはまるのは？\n\nA：疲れが続くとじわじわ出る\nB：急に強く出る／ピリピリ・赤みが強い\nC：特に思い当たらない`,
-    buttons: [
+    altText: '【質問1】ここ数年間の体力傾向',
+    header: '【Q1】ここ数年間の体力傾向（共通質問）',
+    body: `Q1：風邪（インフルエンザ等強い感染症除く）をひいた時、あなたに多いパターンは？\n\nA：軽い症状で長引く／ダラダラ治りきらない\nB：ガッと発症してもすぐ治る／滅多に風邪はひかない（特別な対策なし）\nC：時と場合による／特に傾向はない`,    buttons: [
       { label: 'A', data: 'skin_Q1_A' },
       { label: 'B', data: 'skin_Q1_B' },
       { label: 'C', data: 'skin_Q1_C' },
