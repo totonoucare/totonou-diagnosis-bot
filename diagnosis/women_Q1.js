bba9d68b2a2e727194488474df66a1ec66b802cc
totonoucare <novelinks9@gmail.com>
2025-06-10 11:37:40 +0900

    Update women_Q1.js

--- diagnosis/women_Q1.js
+++ diagnosis/women_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function women_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】生理前後の不調について',
     header: '【Q1】生理のタイミングと不調',
-    body: `Q1：生理前後で、不調が辛いのは？\n\nA：月経前にイライラ・張り・むくみ\nB：月経後にだるさ・ふらつき・抜け毛\nC：どちらも特にない`,
+    body: `Q1：生理前後で、不調が辛いのは？\n\nA：月経後にだるさ・ふらつき・抜け毛\nB：月経前にイライラ・張り・むくみ\nC：どちらも特にない`,
     buttons: [
       { label: 'A', data: 'women_Q1_A' },
       { label: 'B', data: 'women_Q1_B' },
