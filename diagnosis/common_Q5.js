faa6713b245c553e704235cc9bf4b386efafad36
totonoucare <novelinks9@gmail.com>
2025-06-13 10:10:55 +0900

    Update common_Q5.js

--- diagnosis/common_Q5.js
+++ diagnosis/common_Q5.js
@@ -2,8 +2,8 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function common_Q5() {
   const flex = MessageBuilder({
-    altText: '【質問5】体の違和感・張り感がある動き',
-    header: '【Q5】体の違和感・張り感がある動き',
+    altText: '【質問5】経絡（内臓）の状態',
+    header: '【Q5】経絡（けいらく）・内臓の負担',
     body: `Q5：次の動作のうち、体のどこかに一番違和感やツラさを感じる動作を選んでください。\n\nA：首を後ろに倒すor左右に回す\nB：腕をバンザイする\nC：前屈する\nD：腰を左右にねじるor側屈\nE：上体をそらす`,
     buttons: [
       { label: 'A', data: 'common_Q5_A' },
