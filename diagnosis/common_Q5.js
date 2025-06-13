86ac8d107e8b0fdac6e8d6a1e87df892bb27e5e7
totonoucare <novelinks9@gmail.com>
2025-06-13 10:13:36 +0900

    Update common_Q5.js

--- diagnosis/common_Q5.js
+++ diagnosis/common_Q5.js
@@ -3,7 +3,7 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 module.exports = async function common_Q5() {
   const flex = MessageBuilder({
     altText: '【質問5】経絡（内臓）の状態',
-    header: '【Q5】経絡（けいらく）・内臓の負担',
+    header: '【Q5】経絡（内臓膜や筋膜）の負担状況',
     body: `Q5：次の動作のうち、体のどこかに一番違和感やツラさを感じる動作を選んでください。\n\nA：首を後ろに倒すor左右に回す\nB：腕をバンザイする\nC：前屈する\nD：腰を左右にねじるor側屈\nE：上体をそらす`,
     buttons: [
       { label: 'A', data: 'common_Q5_A' },
