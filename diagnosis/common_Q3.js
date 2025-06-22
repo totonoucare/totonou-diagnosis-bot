0ee4c89db012e86eea33030214bef70f03fcd3de
totonoucare <novelinks9@gmail.com>
2025-06-23 02:02:12 +0900

    Update common_Q3.js

--- diagnosis/common_Q3.js
+++ diagnosis/common_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やお身体の状態について',
     header: '【Q3】体力やお身体の状態について',
-    body: `Q3：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：夢が多い／めまい・立ちくらみ／肌・髪が乾燥\nB：疲れやすい／息切れしやすい／声に力がない\nC：どちらもあまり感じない`,
+    body: `Q3：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：肌・髪が乾燥／夢が多い／めまい・立ちくらみ\nB：疲れやすい／息切れしやすい／声に力がない\nC：どれもあまり感じない`,
     buttons: [
       { label: 'A', data: 'common_Q3_A' },
       { label: 'B', data: 'common_Q3_B' },
