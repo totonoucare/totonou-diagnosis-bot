e4acafde7039246081445df46a19915fcc745dc8
totonoucare <novelinks9@gmail.com>
2025-06-27 16:16:05 +0900

    Delete line.js

--- line.js
+++
@@ -1,9 +0,0 @@
-// line.js
-const line = require('@line/bot-sdk');
-
-const client = new line.Client({
-  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.LINE_CHANNEL_SECRET,
-});
-
-module.exports = { client };
