15d0de389f0840366bbb43c82484b5dfde83506f
totonoucare <novelinks9@gmail.com>
2025-06-30 14:31:19 +0900

    Update getUserIdFromLineId.js

--- utils/getUserIdFromLineId.js
+++ utils/getUserIdFromLineId.js
@@ -1,7 +1,9 @@
 const { createClient } = require("@supabase/supabase-js");
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
 
-async function getUserIdFromLineId(lineId) {
+async function getUserIdFromLineId(lineIdRaw) {
+  const lineId = lineIdRaw.replace(/\s/g, '').trim(); // ← 念のため全部クリーニング
+
   const { data, error } = await supabase
     .from("users")
     .select("id")
