cb13faad2a289838e67c06016cd06e74658e1a2f
totonoucare <novelinks9@gmail.com>
2025-07-03 21:34:06 +0900

    Update pollen_Q3.js

--- diagnosis/pollen_Q3.js
+++ diagnosis/pollen_Q3.js
@@ -2,9 +2,9 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function pollen_Q3() {
   const flex = MessageBuilder({
-    altText: '【質問3】体力やエネルギー感',
-    header: '【Q3】体力の傾向',
-    body: `Q3：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：夢が多い／めまい・立ちくらみ／肌・髪が乾燥\nB：疲れやすい／息切れしやすい／声に力がない\nC：どちらもあまり感じない`,
+    altText: '【質問3】体力やお身体の状態について',
+    header: '【Q3】体力やお身体の状態について',
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\nB：階段や坂で息が切れる／声が小さい・張れない\nC：特に気になるものはない`,
     buttons: [
       { label: 'A', data: 'pollen_Q3_A' },
       { label: 'B', data: 'pollen_Q3_B' },
