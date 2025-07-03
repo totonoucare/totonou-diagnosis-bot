4c59b00e8da15df0b61c96d35059a66f7588fe76
totonoucare <novelinks9@gmail.com>
2025-07-03 21:39:56 +0900

    Update skin_Q4.js

--- diagnosis/skin_Q4.js
+++ diagnosis/skin_Q4.js
@@ -4,7 +4,7 @@ module.exports = async function skin_Q4() {
   const flex = MessageBuilder({
     altText: '【質問4】肌・頭皮の不調傾向',
     header: '【Q4】肌・頭皮の不調の特徴と傾向',
-    body: `Q4：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：ストレスや月経前(女性)に悪化\nB：ベタつき／吹き出物／重だるさ\nC：目の下のクマ／しみ・くすみ／部分的な抜け毛\nD：特に当てはまらない`,
+    body: `Q4：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：ストレスで悪化／気分によって波がある\nB：ベタつき／吹き出物／重だるさ\nC：目の下のクマ／しみ・くすみ／部分的な抜け毛\nD：特に当てはまらない`,
     buttons: [
       { label: 'A', data: 'skin_Q4_A' },
       { label: 'B', data: 'skin_Q4_B' },
