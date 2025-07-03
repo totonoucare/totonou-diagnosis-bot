6c4a350476798a2c834ba865afcfab28f9d8e5ae
totonoucare <novelinks9@gmail.com>
2025-07-03 21:21:43 +0900

    Update pollen_Q1.js

--- diagnosis/pollen_Q1.js
+++ diagnosis/pollen_Q1.js
@@ -6,9 +6,9 @@ module.exports = async function pollen_Q1() {
     header: '【Q1】ここ数年間の体力傾向（共通質問）',
     body: `Q1：以下のうち、一番近いものを選んでください。\n\nA：予定が続くとぐったりする。寝ても疲れが抜けにくい。\nB：予定が多くても比較的元気。少しの休息ですぐ動ける。\nC：C：予定が多いと疲れはするけど、寝れば戻る`,
     buttons: [
-      { label: 'A', data: 'common_Q1_A' },
-      { label: 'B', data: 'common_Q1_B' },
-      { label: 'C', data: 'common_Q1_C' },
+      { label: 'A', data: 'pollen_Q1_A' },
+      { label: 'B', data: 'pollen_Q1_B' },
+      { label: 'C', data: 'pollen_Q1_C' },
     ],
   });
 
