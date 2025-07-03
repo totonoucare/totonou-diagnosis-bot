3e09f84a25c370efcf91b301a9c92fd0cd6699ed
totonoucare <novelinks9@gmail.com>
2025-07-03 17:05:59 +0900

    Update common_Q3.js

--- diagnosis/common_Q3.js
+++ diagnosis/common_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やお身体の状態について',
     header: '【Q3】体力やお身体の状態について',
-    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：顔色が悪い／髪や肌の乾燥・疲れ目／不安感がある\nB：軽い運動(階段・散歩)ですぐ息が上がる／話し声が力ない\nC：どれも気になるほどではない`,
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\nB：階段や坂で息が切れる／声が小さい・張れない\nC：特に気になるものはない`,
     buttons: [
       { label: 'A', data: 'common_Q3_A' },
       { label: 'B', data: 'common_Q3_B' },
