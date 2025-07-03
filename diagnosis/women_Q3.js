b1b95c136583a8832cf5301beef1cb7bc7d97395
totonoucare <novelinks9@gmail.com>
2025-07-03 21:26:11 +0900

    Update women_Q3.js

--- diagnosis/women_Q3.js
+++ diagnosis/women_Q3.js
@@ -2,10 +2,9 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 
 module.exports = async function women_Q3() {
   const flex = MessageBuilder({
-    altText: '【質問3】普段の体調や見た目',
-    header: '【Q3】日常の体調変化',
-    body: `Q3：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：肌や髪がパサつく／めまい・ふらつき\nB：顔色が白い／疲れやすい／声が小さい\nC：どちらも特にない`,
-    buttons: [
+    altText: '【質問3】体力やお身体の状態について',
+    header: '【Q3】体力やお身体の状態について',
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\nB：階段や坂で息が切れる／声が小さい・張れない\nC：特に気になるものはない`,    buttons: [
       { label: 'A', data: 'women_Q3_A' },
       { label: 'B', data: 'women_Q3_B' },
       { label: 'C', data: 'women_Q3_C' },
