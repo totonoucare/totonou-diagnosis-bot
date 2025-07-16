793f27a0e73e2290efa490aa962a27ecc4a68e4c
totonoucare <novelinks9@gmail.com>
2025-07-16 22:45:56 +0900

    Create typeImageDictionary.js

---
+++ diagnosis/typeImageDictionary.js
@@ -0,0 +1,29 @@
+const typeImageDictionary = {
+  "虚寒・気血両虚タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kyokan.png",
+  "陽虚傾向タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-youkyo-leaning.png",
+  "陽虚（本格型）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-youkyo-true.png",
+  "血虚タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kekkyo.png",
+  "単純気虚タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kikyo-simple.png",
+  "気血両虚タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-ryokyo.png",
+  "陰虚（虚熱）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-inyo-kyonetsu.png",
+  "鬱熱（繊細型）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-utsunetsu-sensitive.png",
+  "寒・瘀血タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kan-oketsu.png",
+  "単純寒証タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kan-simple.png",
+  "血虚傾向タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kekkyo-leaning.png",
+  "平性（偏りなし）": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-heisei.png",
+  "気虚タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kikyo.png",
+  "陰虚タイプ（代謝高め）": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-inyo-highmetabolism.png",
+  "気滞熱タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kitai-netu.png",
+  "鬱熱タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-utsunetsu.png",
+  "実寒・瘀血タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-jitsukan-oketsu.png",
+  "実寒（外因性）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-jitsukan-gaiin.png",
+  "実寒・水滞タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-jitsukan-suitai.png",
+  "血虚（過活動型）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kekkyo-overactive.png",
+  "体力充実タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-tairyoku.png",
+  "気虚（過活動型）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-kikyo-overactive.png",
+  "陰虚火旺タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-inyo-kaou.png",
+  "実熱タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-jitsunetsu.png",
+  "鬱熱（過活動型）タイプ": "https://totonoucare.com/wp-content/themes/totonoucare/images/type-utsunetsu-overactive.png"
+};
+
+module.exports = typeImageDictionary;
