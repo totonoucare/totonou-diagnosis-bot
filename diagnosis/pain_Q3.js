3dd8a62393750fbf13c206de35c8835cb5794ac2
totonoucare <novelinks9@gmail.com>
2025-07-24 02:51:38 +0900

    Update pain_Q3.js

--- diagnosis/pain_Q3.js
+++ diagnosis/pain_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function pain_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やお身体の状態について',
     header: '【Q3】体力やお身体の状態について',
-    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\n\nB：階段や坂で息が切れる／声が小さい・張れない\n\nC：特に気になるものはない`,
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\n\nB：疲れやすい・息切れしやすい／声が小さい・張れない\n\nC：特に気になるものはない`,
     buttons: [
       { label: 'A', data: 'pain_Q3_A' },
       { label: 'B', data: 'pain_Q3_B' },
