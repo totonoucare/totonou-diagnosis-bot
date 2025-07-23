0bde69f412c39ab92b6fa6c2296e441eb5036c42
totonoucare <novelinks9@gmail.com>
2025-07-23 12:52:10 +0900

    Update women_Q3.js

--- diagnosis/women_Q3.js
+++ diagnosis/women_Q3.js
@@ -4,7 +4,8 @@ module.exports = async function women_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やお身体の状態について',
     header: '【Q3】体力やお身体の状態について',
-    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\nB：階段や坂で息が切れる／声が小さい・張れない\nC：特に気になるものはない`,    buttons: [
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：目が疲れやすい／肌や髪が乾燥しがち／不安になりやすい\n\nB：階段や坂で息が切れる／声が小さい・張れない\n\nC：特に気になるものはない`, 
+    buttons: [
       { label: 'A', data: 'women_Q3_A' },
       { label: 'B', data: 'women_Q3_B' },
       { label: 'C', data: 'women_Q3_C' },
