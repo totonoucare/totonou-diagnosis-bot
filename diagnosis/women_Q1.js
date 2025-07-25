1cfcde7c832a3eb95932ff2f662d27adc4eb5ead
totonoucare <novelinks9@gmail.com>
2025-07-25 10:39:37 +0900

    Update women_Q1.js

--- diagnosis/women_Q1.js
+++ diagnosis/women_Q1.js
@@ -4,7 +4,7 @@ module.exports = async function women_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】ここ数年間の体力傾向',
     header: '【Q1】ここ数年間の体力傾向（共通質問）',
-    body: `Q1：以下のうち、一番近いものを選んでください。\n\nA：予定が続くとぐったりする。寝ても疲れが抜けにくい。\n\nB：予定が多くても比較的元気。少しの休息ですぐ動ける。\n\nC：C：予定が多いと疲れはするけど、寝れば戻る`,
+    body: `Q1：以下のうち、一番近いものを選んでください。\n\nA：予定が続くとぐったりする。寝ても疲れが抜けにくい。\n\nB：予定が多くても比較的元気。少しの休息ですぐ動ける。\n\nC：予定が多いと疲れはするけど、寝れば戻る`,
     buttons: [
       { label: 'A', data: 'women_Q1_A' },
       { label: 'B', data: 'women_Q1_B' },
