fca60a59ff00a61a783032cd5d03c6b3f776ec85
totonoucare <novelinks9@gmail.com>
2025-07-25 11:42:59 +0900

    Update women_Q2.js

--- diagnosis/women_Q2.js
+++ diagnosis/women_Q2.js
@@ -4,7 +4,7 @@ module.exports = async function women_Q2() {
   const flex = MessageBuilder({
     altText: '【質問2】冷え・のぼせ',
     header: '【Q2】冷えやのぼせの傾向',
-    body: `Q2：次のうち、当てはまる状態をより多く（または強く）含む選択肢はどれですか？\n\nA：全身が冷える／下腹部が冷たい\n\nB：上半身や顔がほてる／寝汗・のぼせ・イライラ\n\nC：とくに感じない`,
+    body: `Q2：気温や環境について、普段どのように感じますか？\n\nA：寒がりで、少し冷えるだけで厚着したくなる\n\nB：暑がり(またはほてり・のぼせ)で、すぐに上着を脱ぎたくなる\n\nC：暑さ・寒さは同じくらいツラい、またはどちらもあまり気にならない`,
     buttons: [
       { label: 'A', data: 'women_Q2_A' },
       { label: 'B', data: 'women_Q2_B' },
