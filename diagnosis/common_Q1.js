b31bf67bfd727b5bf78af8260e837c1d5b3e5b9b
totonoucare <novelinks9@gmail.com>
2025-07-25 12:22:18 +0900

    Update common_Q1.js

--- diagnosis/common_Q1.js
+++ diagnosis/common_Q1.js
@@ -3,7 +3,7 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 module.exports = async function common_Q1() {
   const flex = MessageBuilder({
     altText: '【質問1】ここ数年間の体力傾向',
-    header: '【Q1】ここ数年間の体力傾向（共通質問）',
+    header: '【Q1】ここ数年間の体力傾向',
     body: `Q1：以下のうち、一番近いものを選んでください。\n\nA：予定が続くとぐったりする。寝ても疲れが抜けにくい。\n\nB：予定が多くても比較的元気。少しの休息ですぐ動ける。\n\nC：予定が多いと疲れはするけど、寝れば戻る`,
     buttons: [
       { label: 'A', data: 'common_Q1_A' },
