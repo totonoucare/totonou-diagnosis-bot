522cc13871a9598c4e5a91301001baf8ab9ae0e3
totonoucare <novelinks9@gmail.com>
2025-09-09 18:45:14 +0900

    Update common_Q3.js

--- diagnosis/common_Q3.js
+++ diagnosis/common_Q3.js
@@ -4,7 +4,7 @@ module.exports = async function common_Q3() {
   const flex = MessageBuilder({
     altText: '【質問3】体力やお身体の状態について',
     header: '【Q3】体力やお身体の状態について',
-    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：肌や髪が乾燥／目のかすみ／不安になりやすい\n\nB：息切れ・声が小さい・食後にだるい・力が入りにくい（気虚）\n\nC：特に気になるものはない`,
+    body: `Q3：次のうち、気になる状態を多く（または強く）含む選択肢はどれですか？\n\nA：肌や髪が乾燥／目のかすみ／不安を感じやすい\n\nB：息切れ・声が小さい・食後にだるい・力が入りにくい（気虚）\n\nC：特に気になるものはない`,
     buttons: [
       { label: 'A', data: 'common_Q3_A' },
       { label: 'B', data: 'common_Q3_B' },
