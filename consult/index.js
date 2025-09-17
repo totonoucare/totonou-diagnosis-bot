12f19bd204c0ee199c2e9c1e68bce109fc73bdac
totonoucare <novelinks9@gmail.com>
2025-09-17 16:21:37 +0900

    Update index.js

--- consult/index.js
+++ consult/index.js
@@ -90,7 +90,7 @@ module.exports = async function consult(event, client) {
       model: "gpt-4o",
       messages,
       max_completion_tokens: 700,
-      temperature: 0.8,
+      temperature: 0.85,
     });
 
     const text =
