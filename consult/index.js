8ff33dc5533a5f167616b81100b3cda61c1e2846
totonoucare <novelinks9@gmail.com>
2025-10-21 15:32:50 +0900

    Update index.js

--- consult/index.js
+++ consult/index.js
@@ -87,7 +87,7 @@ module.exports = async function consult(event, client) {
   // 生成＆返信
   try {
     const rsp = await openai.chat.completions.create({
-      model: "gpt-5",
+      model: "gpt-4o",
       messages,
     });
 
