c2171c4a6aabdd8e7d776282705054910383385b
totonoucare <novelinks9@gmail.com>
2025-10-23 23:58:57 +0900

    Update flexBuilder.js

--- utils/flexBuilder.js
+++ utils/flexBuilder.js
@@ -839,7 +839,7 @@ function buildReminderFlexFromText(text) {
       type: "bubble",
       hero: {
         type: "image",
-        url: "https://totonoucare.com/images/flex/reminder-hero.jpg",
+        url: "https://totonoucare.com/wp-content/themes/totonoucare/images/hero-pc.png",
         size: "full",
         aspectMode: "cover",
         aspectRatio: "16:9"
