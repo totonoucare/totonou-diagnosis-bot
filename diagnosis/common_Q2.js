2b8e853ac728e7bdb4f942032bfbfcf3559bb260
totonoucare <novelinks9@gmail.com>
2025-12-03 16:20:34 +0900

    Update common_Q2.js

--- diagnosis/common_Q2.js
+++ diagnosis/common_Q2.js
@@ -3,8 +3,7 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 module.exports = async function common_Q2() {
   const flex = MessageBuilder({
     altText: '【質問2】冷えや暑さについて',
-    header: '【Q2】環境温度の感じ方',
-    header: '【Q2】環境温度の感じ方(寒/熱)',
+    header: '【Q2】環境温度の感じ方について',
     body: `Q2：気温や環境について、普段どのように感じますか？（A・B両方の場合、より辛く感じる方を選んでください。）\n\nA：寒がりで、冷えると不調を感じやすい（寒）\n\nB：暑がりで、ほてり・のぼせが出やすい（熱）\n\nC：寒さも暑さもあまり気にならない（平性）`,
     buttons: [
       { label: 'A', data: 'common_Q2_A' },
