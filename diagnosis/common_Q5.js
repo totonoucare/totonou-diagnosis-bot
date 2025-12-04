7380348d64ea70c53f58b978b9900ca4a39ef473
totonoucare <novelinks9@gmail.com>
2025-12-04 10:35:22 +0900

    Update common_Q5.js

--- diagnosis/common_Q5.js
+++ diagnosis/common_Q5.js
@@ -3,7 +3,7 @@ const { MessageBuilder } = require('../utils/flexBuilder');
 module.exports = async function common_Q5() {
   const flex = MessageBuilder({
     altText: '【質問5】経絡（内臓）の状態',
-    header: '【Q5】経絡（けいらく）ラインの負担状況',
+    header: '【Q5】経絡（けいらく）ラインの負荷テスト',
     body: `Q5：次の動作をすることで、体のどこかに違和感やツラさを感じる動作を選んでください。\n※複数ある場合は最もツラい動作を選んでください。\n\nA：首を後ろに倒す・横(左右)を向く\n\nB：腕をバンザイ・首をうつむける\n\nC：前屈する(立った状態で)\n\nD：腰を左右にひねる・側屈する\n\nE：腰に手を当て上体そらし`,
     buttons: [
       { label: 'A', data: 'common_Q5_A' },
