5ec9894a3c1764a968f9fb5837130efd233b50f4
totonoucare <novelinks9@gmail.com>
2025-12-12 14:01:59 +0900

    Update questionSets.js

--- followup/questionSets.js
+++ followup/questionSets.js
@@ -8,7 +8,7 @@
 const followupQuestionSet = [
   {
     id: "Q1",
-    header: "【Q1】🌡 主なお悩みのツラさ",
+    header: "【Q1】🌡 主なお悩みのととのい",
     body:
       "いま一番気になるお悩み\n「{{symptom}}」\n\nのツラさを、\n「1＝気にならない／かなりラク」\n「5＝かなりツラい」\nで選んでください。",
     isMulti: true,
@@ -22,7 +22,7 @@ const followupQuestionSet = [
   },
   {
     id: "Q2",
-    header: "【Q2】生活リズムの整い具合",
+    header: "【Q2】Q2：Q2：🔄 心身コンディションのととのい",
     body:
       "最近の状態をそれぞれ\n「1＝よく整っている」\n「5＝かなり乱れている」\nで選んでください。",
     isMulti: true,
@@ -46,7 +46,7 @@ const followupQuestionSet = [
   },
   {
     id: "Q3",
-    header: "【Q3】🧍‍♀️ 動作の負荷チェック",
+    header: "【Q3】🧍‍♀️ 体表ラインのととのい",
     body:
       "体質分析で確認した動き\n「{{motion}}」\n\nを、いまやってみたときのツラさを\n「1＝気にならない／かなりラク」\n「5＝かなりツラい」\nで選んでください。\n（※経絡ストレッチと同じ動きでもOK）",
     isMulti: true,
